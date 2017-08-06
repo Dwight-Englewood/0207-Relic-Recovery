a426297e4f0d55f1b0a6f85f04932eb0152bccd2
Burtorustum <aburur@d-e.org>
2017-08-06 17:13:59 -0400

    Added enum for bot actions and the bot class to be filled in.

---
+++ ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bot.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode;
+
+/**
+ * Created by aburur on 8/6/17.
+ */
+
+public class Bot
+{
+    /**
+     * TODO: Fill bot class
+     */
+}
---
+++ ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BotActions.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode;
+
+/**
+ * Created by aburur on 8/6/17.
+ */
+
+public enum BotActions
+{
+    /**
+     * TODO: Create enum for actions of the bot (eg. movement control, etc)
+     */
+}

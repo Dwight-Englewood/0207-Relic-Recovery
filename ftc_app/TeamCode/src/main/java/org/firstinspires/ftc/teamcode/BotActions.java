e77f0d989940258771350459d0c7765645f4aea9
weznon <waterwasp55555@gmail.com>
2017-08-07 18:11:15 -0400

    Added some basic enums; we should add more as we figure out the bot's hardware

--- ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BotActions.java
+++ ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BotActions.java
@@ -4,9 +4,25 @@ package org.firstinspires.ftc.teamcode;
  * Created by aburur on 8/6/17.
  */
 
-public enum BotActions
-{
-    /**
+public class BotActions  {
+    static enum Movement {
+        FORWARD, BACKWARD, LEFTSTRAFE, RIGHTSTRAFE, LEFTTURN, RIGHTTURN;
+        /*
+         * Should be good for now?
+         * We might want to add ones in for diagonals
+         */
+    }
+    static enum InitHardware {
+        DRIVETRAIN, SENSORS, SERVOS
+        /*
+        Barebones stuff for init
+        as we figure out what the bot actually will need we'll add more
+         */
+    }
+
+
+
+    /*
      * TODO: Create enum for actions of the bot (eg. movement control, etc)
      */
 }

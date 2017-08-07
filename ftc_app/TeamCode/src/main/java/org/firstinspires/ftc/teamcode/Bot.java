bdfb5954ac3728e88ac42922469563850aa68249
Burtorustum <aburur@d-e.org>
2017-08-07 16:45:42 -0400

    slight improvements to bot class

--- ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bot.java
+++ ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bot.java
@@ -4,9 +4,34 @@ package org.firstinspires.ftc.teamcode;
  * Created by aburur on 8/6/17.
  */
 
+//TODO: Create bot class
 public class Bot
 {
     /**
-     * TODO: Fill bot class
+     * Motor Declarations
      */
+
+    /**
+     * Servo Declarations
+     */
+
+    /**
+     * Sensor Declarations
+     */
+
+    /**
+     * Variable Declarations
+     */
+
+    /**
+     * Other Declarations
+     */
+
+    /**
+     * Initialization Function
+     */
+    public void initialize()
+    {
+
+    }
 }

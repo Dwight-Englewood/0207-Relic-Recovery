c2e111a89bad7d4b90b3cae71d9e60031c4ec0d8
weznon <waterwasp55555@gmail.com>
2017-09-09 12:33:47 -0400

    Finish updating

---
+++ ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bot.java
@@ -0,0 +1,63 @@
+package org.firstinspires.ftc.teamcode;
+
+/**
+ * Created by aburur on 8/6/17.
+ */
+
+//TODO: Create bot class
+public class Bot
+{
+    /**
+     * Constructor
+     */
+    public Bot()
+    {}
+
+    /**
+     * Motor Declarations
+     */
+
+    /**
+     * Servo Declarations
+     */
+
+    /**
+     * Sensor Declarations
+     */
+
+    /**
+     * Variable Declarations
+     */
+
+    /**
+     * Other Declarations
+     */
+
+    /**
+     * Initialization Function
+     */
+    public void initialize()
+    {
+
+    }
+
+    /**
+     * Movement Functions
+     */
+
+    /**
+     * Action Functions
+     */
+
+    /**
+     * Sensor-Related Functions
+     */
+
+    /**
+     * Getter Functions
+     */
+
+    /**
+     * Helper Functions
+     */
+}

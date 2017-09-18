2f7d080ef396379e4c59814625d7dfc7712806b6
Burtorustum <aburur@d-e.org>
2017-09-18 08:12:06 -0400

    Some changes to telebop -- started wrok on field centric drive

--- ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Telebop.java
+++ ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Telebop.java
@@ -1,10 +1,37 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
 /**
  * Created by aburur on 9/10/17.
  */
-
-public class Telebop
+@TeleOp(name="Telebop", group="Teleop")
+//@Disabled
+public class Telebop extends OpMode
 {
+        @Override
+        public void init() {
+
+        }
+
+        @Override
+        public void init_loop() {
+        }
+
+        @Override
+        public void start() {
+
+        }
+
+        @Override
+        public void loop() {
+
+        }
+
+        @Override
+        public void stop() {
+
+        }
 
 }

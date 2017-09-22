f707fe11f30124daa954bdbcc110e6fd75a318cf
Burtorustum <aburur@d-e.org>
2017-09-22 10:07:42 -0400

    lol field centric drive in 2017

--- ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldCentricDrive.java
+++ ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldCentricDrive.java
@@ -1,8 +1,39 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
 /**
  * Created by aburur on 9/20/17.
  */
 
-public class FieldCentricDrive {
+@TeleOp(name="FieldCentricDrive", group="Testing")
+//@Disabled
+public class FieldCentricDrive extends OpMode
+{
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void init_loop() {
+
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
 }

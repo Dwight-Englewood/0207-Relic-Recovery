9d6d13317af4e814a757f0497bee621b86dee688
weznon <waterwasp55555@gmail.com>
2017-09-29 19:00:14 -0400

    removed extra created by comment

--- ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopAutobalance.java
+++ ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopAutobalance.java
@@ -6,9 +6,6 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-/**
- * Created by weznon on 9/29/17.
- */
 @TeleOp(name = "TeleopAutobalance", group = "Teleop")
 public class TeleopAutobalance extends OpMode {
 

322df22091ac08138848b860a8b8ed807e47ce1d
weznon <waterwasp55555@gmail.com>
2017-09-29 18:56:21 -0400

    created the teleopautobalance teleop

---
+++ ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopAutobalance.java
@@ -0,0 +1,42 @@
+package org.firstinspires.ftc.teamcode;
+/**
+ * Created by weznon on 9/29/17.
+ */
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+/**
+ * Created by weznon on 9/29/17.
+ */
+@TeleOp(name = "TeleopAutobalance", group = "Teleop")
+public class TeleopAutobalance extends OpMode {
+
+    private Bot robot = new Bot();
+
+    @Override
+    public void init() {
+        telemetry.addData("Status", "Initialized");
+        robot.init(hardwareMap, telemetry);
+        telemetry.update();
+    }
+
+    @Override
+    public void init_loop() {
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void loop() {
+        
+    }
+
+    @Override
+    public void stop() {
+
+    }
+}

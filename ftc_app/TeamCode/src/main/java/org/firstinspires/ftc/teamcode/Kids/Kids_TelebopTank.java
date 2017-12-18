1aba20bdff6f06b005396d2aabc35cc1019ecf65
Wen <waterwasp55555@gmail.com>
2017-12-18 15:18:20 -0500

    Disabled the outreach teleops

--- ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Kids/Kids_TelebopTank.java
+++ ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Kids/Kids_TelebopTank.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.Kids;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -11,6 +12,7 @@ import org.firstinspires.ftc.teamcode.Enums.ReleasePosition;
  * Created by aburur on 12/12/17.
  */
 @TeleOp(name = "KIDS TANK", group = "Teleop")
+@Disabled
 public class Kids_TelebopTank extends OpMode
 {
     Bot robot = new Bot();

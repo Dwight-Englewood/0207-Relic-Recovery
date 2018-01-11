b0efda22b310413fa71f1b5d137cf07c65e65875
Burtorustum <aburur@d-e.org>
2018-01-11 08:47:58 -0500

    Updated Sensor test to include range

--- ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/SensorTest.java
+++ ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/SensorTest.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.Testing;
 
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cRangeSensor;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -17,6 +18,7 @@ import org.firstinspires.ftc.teamcode.Bot;
 //@Disabled
 public class SensorTest extends OpMode {
     Bot robot = new Bot();
+
     public void init() {
         robot.init(hardwareMap);
     }
@@ -35,10 +37,11 @@ public class SensorTest extends OpMode {
         /*telemetry.addData("Left CS red", robot.cryptoColorL.red());
         telemetry.addData("Left CS blue", robot.cryptoColorL.blue());
         telemetry.addData("Right CS red", robot.cryptoColorR.red());
-        telemetry.addData("Right CS blue", robot.cryptoColorR.blue());
-        telemetry.addData("range", robot.rangeSensor.getDistance(DistanceUnit.CM));
-        telemetry.addData("range Optical", robot.rangeSensor.cmOptical());
-        telemetry.addData("range ultrasonic", robot.rangeSensor.cmUltrasonic());*/
+        telemetry.addData("Right CS blue", robot.cryptoColorR.blue()); */
+
+        telemetry.addData("range", robot.rangeBack.getDistance(DistanceUnit.CM));
+        telemetry.addData("range Optical", robot.rangeBack.cmOptical());
+        telemetry.addData("range ultrasonic", robot.rangeBack.cmUltrasonic());
         telemetry.addData("jewel red", robot.colorSensor.red());
         telemetry.addData("jewel blue", robot.colorSensor.blue());
 

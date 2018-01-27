b563c04af229f9f0c70c4b2212a916fe215d66f0
Wen <waterwasp55555@gmail.com>
2018-01-26 19:30:07 -0500

    documentation, minor change

--- ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utility/EnumController.java
+++ ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utility/EnumController.java
@@ -3,6 +3,9 @@ package org.firstinspires.ftc.teamcode.Utility;
 import java.util.ArrayList;
 
 /**
+ * The EnumController is an object which allows for easier handling of state machines(?)
+ *
+ *
  * Created by weznon on 11/27/17.
  */
 
@@ -13,18 +16,30 @@ public class EnumController<T> {
     public ArrayList<T> instruction;
     public ArrayList<Integer> priorities;
 
-
+    /**
+     * Constructor
+     * Takes a defaultVal, which is what EnumController#process will return if nothing is added to its internal lists
+     */
     public EnumController(T defaultVal) {
         this.defaultVal = defaultVal;
         this.instruction = new ArrayList<>();
         this.priorities = new ArrayList<>();
     }
 
+    /**
+     * Handles the addition of values and the associated priority
+     * in EnumController#process, it is assumed the lists are the same length. Therefore, we only have one add method, which forces this to be true
+     */
     public void addInstruction(T newVal, int Flag) {
         this.instruction.add(newVal);
         this.priorities.add(Flag);
     }
 
+    /** 
+     * This function will iterate over the lists and take the object with the highest priority
+     * The exception is when the priority is negative, which will break the loop. This has applications in instances if we must be sure that something exits immedediately, as we an unsure what the largest priority passed in will be
+     *
+     */
     public T process() {
         if (this.instruction.size() == 0) {
             return this.defaultVal;
@@ -44,9 +59,16 @@ public class EnumController<T> {
 
         return (this.currentVal);
     }
-
+    
+    /**
+     * empties the lists, to avoid conflicts with future instances of process
+     */
     public void reset() {
         this.currentVal = defaultVal;
+        for (int i = 0; i < this.instruction.size(); i++) {
+            this.instruction.remove(0);
+            this.priorities.remove(0);
+        }
     }
 
 }

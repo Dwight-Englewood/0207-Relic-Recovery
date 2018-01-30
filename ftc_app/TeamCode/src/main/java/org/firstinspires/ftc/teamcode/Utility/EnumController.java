e61597f406ab84e1068bc64d87669bac41e6b163
Burtorustum <burtorustum@gmail.com>
2018-01-29 19:54:51 -0500

    Merge branch 'master' of https://github.com/Dwight-Englewood-Robotics/0207-Relic-Recovery
    # Conflicts:
    #	ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Telebop.java

--- ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utility/EnumController.java
+++ ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utility/EnumController.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 
 /**
  * The EnumController is an object which allows for easier handling of state machines(?)
- *
- *
  * Created by weznon on 11/27/17.
  */
 
@@ -37,8 +35,9 @@ public class EnumController<T> {
 
     /** 
      * This function will iterate over the lists and take the object with the highest priority
-     * The exception is when the priority is negative, which will break the loop. This has applications in instances if we must be sure that something exits immedediately, as we an unsure what the largest priority passed in will be
-     *
+     * The exception is when the priority is negative, which will break the loop.
+     * This has applications in instances if we must be sure that something exits immedediately,
+     * as we an unsure what the largest priority passed in will be
      */
     public T process() {
         if (this.instruction.size() == 0) {

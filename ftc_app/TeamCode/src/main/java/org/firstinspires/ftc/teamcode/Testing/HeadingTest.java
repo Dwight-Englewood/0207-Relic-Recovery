39cca2760ae7a8d1f618ac244d866ddcf7fdfcd3
Burtorustum <burtorustum@gmail.com>
2018-02-03 16:47:06 -0500

    updated heading test

--- ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/HeadingTest.java
+++ ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/HeadingTest.java
@@ -32,7 +32,7 @@ public class HeadingTest extends OpMode {
 
     @Override
     public void loop() {
-       robot.adjustHeading(0, true, telemetry);
+       robot.adjustHeading(0, false, telemetry);
     }
 
 

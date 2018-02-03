5fd9f28d89a88630cc096ed35d04e04426fd3a90
Wen <waterwasp55555@gmail.com>
2018-02-02 19:17:13 -0500

    created a RelicArm test program

---
+++ ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/RelicArmTest.java
@@ -0,0 +1,50 @@
+package org.firstinspires.ftc.teamcode.Testing;
+/**
+ * Created by weznon on 2/2/18.
+ */
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+
+
+@TeleOp(name = "RelicArmTest", group = "Teleop")
+public class RelicArmTest extends OpMode {
+    private CRServo motor1, motor2;
+    @Override
+    public void init() {
+        hardwareMap.get(CRServo.class, "motor1");
+        hardwareMap.get(CRServo.class, "motor2");
+
+    }
+
+    @Override
+    public void init_loop() {
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void loop() {
+        if (gamepad1.b) {
+            motor1.setPower(.5);
+            motor2.setPower(.5);
+        } else if (gamepad1.a) {
+            motor1.setPower(-.5);
+            motor2.setPower(-.5);
+        } else {
+            motor1.setPower(0);
+            motor2.setPower(0);
+        }
+
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+}

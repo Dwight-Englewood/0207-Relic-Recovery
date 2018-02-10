4081dfbd56a57a72c43af7de68bfbf1190049a3e
Wen <waterwasp55555@gmail.com>
2018-02-10 14:23:59 -0500

    made teleop

---
+++ ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Telebop/TelebopRelic.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode.Telebop;
+/**
+ * Created by weznon on 2/10/18.
+ */
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+
+@TeleOp(name = "TelebopRelic", group = "Teleop")
+public class TelebopRelic extends OpMode {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void init_loop() {
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+}

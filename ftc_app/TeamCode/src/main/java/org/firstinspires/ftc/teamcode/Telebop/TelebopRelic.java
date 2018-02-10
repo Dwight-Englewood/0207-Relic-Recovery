466959a81dd0879055ac6dcb5b84152c88778845
weznon <waterwasp55555@gmail.com>
2018-02-10 14:28:09 -0500

    made a telelop which only controls the relic arm

--- ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Telebop/TelebopRelic.java
+++ ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Telebop/TelebopRelic.java
@@ -5,13 +5,22 @@ package org.firstinspires.ftc.teamcode.Telebop;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 
 @TeleOp(name = "TelebopRelic", group = "Teleop")
 public class TelebopRelic extends OpMode {
+
+    CRServo vexMotor1;
+    CRServo vexMotor2;
+
     @Override
     public void init() {
-
+        vexMotor1 = hardwareMap.get(CRServo.class, "rav1");
+        vexMotor2 = hardwareMap.get(CRServo.class, "rav2");
+        vexMotor2.setDirection(CRServo.Direction.FORWARD);
+        vexMotor1.setDirection(CRServo.Direction.FORWARD);
     }
 
     @Override
@@ -25,6 +34,21 @@ public class TelebopRelic extends OpMode {
 
     @Override
     public void loop() {
+
+        if (gamepad2.a) {
+            vexMotor2.setPower(1);
+            vexMotor1.setPower(1);
+            telemetry.addData("power", 1);
+        } else if (gamepad2.y) {
+            vexMotor1.setPower(-1);
+            vexMotor2.setPower(-1);
+            telemetry.addData("power", -1);
+        } else {
+            vexMotor1.setPower(0);
+            vexMotor2.setPower(0);
+            telemetry.addData("power", 0);
+        }
+
 
     }
 

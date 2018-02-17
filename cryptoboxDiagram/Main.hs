5669d33e4ea23492ff0cba4082208b6a9ffbffc0
Wen <waterwasp55555@gmail.com>
2018-02-16 19:53:11 -0500

    changed backend to diagrams from gloss, since gloss cant output pictures

--- cryptoboxDiagram/Main.hs
+++ cryptoboxDiagram/Main.hs
@@ -1,27 +1,12 @@
-{-# LANGUAGE BangPatterns     #-}
-{-# LANGUAGE RecordWildCards  #-}
-
 module Main where
 
-import           Args
-import Graphics.Gloss as G
-
---main:: IO ()
---main = runWithArgs $ \args@Args{..} -> do
-  --putStrLn $ show args
-
-main :: IO()
-main = do
-    display window white (pictures [(translate 300 300 (createGlyph True)), (createGlyph False)])
-
-window :: Display
-window = G.InWindow "test" (1000, 1000) (100, 100)
-
-drawing :: Picture
-drawing = G.rectangleSolid 100 100
+import Diagrams.Prelude
+import Diagrams.Backend.SVG.CmdLine
 
 
-createGlyph :: Bool -> Picture
-createGlyph a = if a then (color (makeColorI 135 88 56 100) (rectangleSolid 100 100)) else (color (makeColorI 119 119 119 100) (rectangleSolid 100 100))
+myCircle :: Diagram B
+myCircle = circle 1
+main = mainWith myCircle
+
 
 

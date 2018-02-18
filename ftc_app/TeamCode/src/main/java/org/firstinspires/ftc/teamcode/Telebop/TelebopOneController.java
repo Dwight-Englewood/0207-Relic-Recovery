dd437d93612fbcc012d3089d4ddb8040f774f65a
Burtorustum <burtorustum@gmail.com>
2018-02-18 00:34:20 -0500

    Began rewrite of sudo telebop

--- ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Telebop/TelebopOneController.java
+++ ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Telebop/TelebopOneController.java
@@ -1,41 +1,22 @@
 package org.firstinspires.ftc.teamcode.Telebop;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
 
 import org.firstinspires.ftc.teamcode.Utility.Bot;
 import org.firstinspires.ftc.teamcode.Utility.MovementEnum;
-import org.firstinspires.ftc.teamcode.Utility.ReleasePosition;
 
 /**
  * Created by plotnw on 11/21/17.
  */
 
-//This is out of date, as the normal telebop is the teleop where the changes will be made
-//
-
-@TeleOp(name = "sudo Telebop", group = "Teleop")
-@Disabled
+@TeleOp(name = "single user mode", group = "Teleop")
+//@Disabled
 public class TelebopOneController extends OpMode {
     Bot robot = new Bot();
-    boolean brakeToggle = false;
-
-    int countdown = 0;
-    int ticks = 0;
-    int wallCountdown = 0;
-
-    ReleasePosition currentPosition = ReleasePosition.MIDDLE;
-    boolean abnormalReleaseFlag = false;
-    boolean i = false;
-
 
     @Override
     public void init() {
-        robot.init(hardwareMap);
-        robot.setDriveMotorModes(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        robot.colorSensor.enableLed(false);
     }
 
     @Override
@@ -44,101 +25,12 @@ public class TelebopOneController extends OpMode {
 
     @Override
     public void start() {
-        telemetry.clear();
-        robot.jewelUp();
-        robot.setDriveMotorModes(DcMotor.RunMode.RUN_USING_ENCODER);
-        ticks = robot.lift.getCurrentPosition();
     }
 
     @Override
     public void loop()
-
     {
-        abnormalReleaseFlag = false;
-        currentPosition = ReleasePosition.MIDDLE;
 
-        if (gamepad1.left_bumper && countdown <= 0) {
-            brakeToggle = !brakeToggle;
-            countdown = 30;
-        }
-        robot.tankDrive(gamepad1.left_stick_y, gamepad1.right_stick_y, gamepad1.left_trigger, gamepad1.right_trigger, i, brakeToggle); // Tank drive???
-        //robot.fieldCentricDrive(gamepad1.left_stick_x, gamepad1.left_stick_y, gamepad1.right_stick_x, gamepad1.left_trigger, gamepad1.right_trigger, brakeToggle);
-
-        if (gamepad1.right_bumper) {
-            abnormalReleaseFlag = true;
-            currentPosition = ReleasePosition.DOWN;
-            robot.intake(1);
-        } else if (gamepad1.right_trigger > .3) {
-            abnormalReleaseFlag = true;
-            currentPosition = ReleasePosition.DOWN;
-            robot.intake(-1);
-        } else {
-            if (!abnormalReleaseFlag) {
-                currentPosition = ReleasePosition.MIDDLE;
-            }
-            robot.intake(0);
-        }
-
-        if (gamepad1.dpad_left) {
-            robot.intakeDrop.setPower(-1);
-        } else if (gamepad1.dpad_right) {
-            robot.intakeDrop.setPower(1);
-        } else {
-            robot.intakeDrop.setPower(0);
-        }
-
-        if (gamepad1.b) {
-            robot.flipUp();
-        } else if (!gamepad1.a) {
-            robot.flipDown();
-        }
-
-        if (gamepad1.dpad_down) {
-            abnormalReleaseFlag = true;
-            currentPosition = ReleasePosition.MIDDLEUP;
-            robot.lift.setPower(-.5);
-        } else if (gamepad1.dpad_up) {
-            abnormalReleaseFlag = true;
-            currentPosition = ReleasePosition.MIDDLEUP;
-            robot.lift.setPower(1);
-        } else {
-            if (!abnormalReleaseFlag) {
-                currentPosition = ReleasePosition.MIDDLE;
-            }
-            robot.lift.setPower(0);
-        }
-
-        if (!abnormalReleaseFlag) {
-            //if (robot.lift.getCurrentPosition() - ticks < 100) {
-            //currentPosition = ReleasePosition.MIDDLEUP;
-            //} else {
-            currentPosition = ReleasePosition.MIDDLE;
-            //}
-        }
-
-        if (gamepad1.y) {
-            currentPosition = ReleasePosition.UP;
-            robot.flipUp();
-            robot.backIntakeWallDown();
-            wallCountdown = 20;
-        } else if (wallCountdown <= 0 && !abnormalReleaseFlag) {
-            currentPosition = ReleasePosition.MIDDLE;
-            robot.backIntakeWallUp();
-        }
-
-        if (gamepad1.x) {
-            robot.jewelServoBottom.setPosition(.3);
-        } else {
-            robot.jewelUp();
-        }
-
-        countdown--;
-        wallCountdown--;
-        robot.releaseMove(currentPosition);
-
-        telemetry.addData("release pos", currentPosition);
-        telemetry.addData("Braking", brakeToggle);
-        telemetry.update();
     }
 
     @Override

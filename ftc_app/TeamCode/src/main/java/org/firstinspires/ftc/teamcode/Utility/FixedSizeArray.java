e0a59f265e31e417546668acf5bdb21b1e117fe9
Wen <waterwasp55555@gmail.com>
2018-02-19 17:24:40 -0500

    made a really bad array that extends list for ease of use

---
+++ ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utility/FixedSizeArray.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode.Utility;
+
+import java.util.ArrayList;
+
+/**
+ * Created by weznon on 2/19/18.
+ */
+
+public class FixedSizeArray extends ArrayList {
+
+    @Deprecated
+    @Override
+    public boolean add(Object object) {
+        return false;
+    }
+
+    @Deprecated
+    @Override
+    public void add(int index, Object object) {
+
+    }
+
+    @Deprecated
+    @Override
+    public Object remove(int index) {
+        return new Object();
+        //lol
+    }
+
+
+}

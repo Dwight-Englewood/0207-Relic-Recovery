3cd76e7965725cb714571e8b35678a15298bd329
Burtorustum <burtorustum@gmail.com>
2018-04-11 10:11:57 -0400

    WORLDOS TELEBOP (only the shell)

---
+++ ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Telebop/Worlds_Telebop.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode.Telebop;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+
+@TeleOp(name = "Worlds_Telebop", group = "Teleop")
+public class Worlds_Telebop extends OpMode {
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void init_loop() {
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+}

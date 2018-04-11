05975e38f48a79825382a7c3de8f3d08c8f7d2c4
Burtorustum <burtorustum@gmail.com>
2018-04-11 14:14:08 -0400

    Added 2 booleans + a process function to be used in conjunction with the enum controller class

--- ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utility/GlyphClamps.java
+++ ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utility/GlyphClamps.java
@@ -7,6 +7,7 @@ public class GlyphClamps {
 
     private Servo front, back;
     private HardwareMap hwMap;
+    public boolean frontClamped, backClamped;
 
     public GlyphClamps(HardwareMap hwMap) {
         this.hwMap = hwMap;
@@ -18,14 +19,22 @@ public class GlyphClamps {
         front = this.hwMap.servo.get("clampf");
         back.scaleRange(.1,.9);
         front.scaleRange(.1,.9);
+
+        frontClamped = false;
+        backClamped = false;
     }
 
     public void clampBack(boolean clamp) {
-        back.setPosition(clamp ? 1 : 0);
+        this.back.setPosition(clamp ? 1 : 0);
     }
 
     public void clampFront(boolean clamp) {
-        front.setPosition(clamp ? 1 : 0);
+        this.front.setPosition(clamp ? 1 : 0);
+    }
+
+    public void process() {
+        this.back.setPosition(this.backClamped ? 1 : 0);
+        this.front.setPosition(this.frontClamped ? 1 : 0);
     }
 
 }
